4049f0d20adeb3689ffb23741a6a5eff2817e2c0
Felipe Henrique <felipe.henrique.f.o@gmail.com>
2020-04-20 11:51:29 -0300

    Inserindo mais valores nos arrays de produtos e categorias e limpando a ul de app.js

--- src/components/productList/productList.js
+++ src/components/productList/productList.js
@@ -4,25 +4,30 @@ import './productList.css'
 
 const ProductList = () => {
 
-    const [itens] = useState([['Pasta de dente', 'Shampoo', 'Sabonete'], ['Detergente']])
+    const [itens] = useState([['Pasta de dente', 'Shampoo', 'Sabonete'], ['Detergente'], ['Doritos', 'Ruffles']])
     const [itensCategoriaSelecionada, setItensCategoriaSelecionada] = useState([])
-    const [categorias] = useState(['Produtos higiênicos', 'Produtos de Limpeza'])
+    const [categorias] = useState(['Produtos higiênicos', 'Produtos de Limpeza', 'Salgadinhos'])
     const [categoriasSelecionadas, setCategoriasSelecionadas] = useState('')
 
     useEffect(()=>{
         if(categoriasSelecionadas === categorias[0])
             setItensCategoriaSelecionada(itens[0])
-        else
+        else if(categoriasSelecionadas === categorias[1])
             setItensCategoriaSelecionada(itens[1])
-    }, [categoriasSelecionadas])
+        else if(categoriasSelecionadas === categorias[2])
+            setItensCategoriaSelecionada(itens[2])
+        else
+            setItensCategoriaSelecionada([])
+    }, [categoriasSelecionadas]); {/* Segundo parâmetro do 'useEffect' é uma lista de dependências */}
 
     return(
         <>
             <select onChange={(event) => setCategoriasSelecionadas(event.target.value)}> {/* O evento 'onChange' é do React, ele recebe um evento como parâmetro */}
+                <option value=""></option>
                 {categorias.map((categoria) => (<option key={categoria} value={categoria}>{categoria}</option>))}
             </select>
+            {(itensCategoriaSelecionada && itensCategoriaSelecionada.length != 0) ? (<ul>{itensCategoriaSelecionada.map( (item, index) => (<Product key={item} numero={index} nome={item} />))}</ul>) : null}
             
-            {itensCategoriaSelecionada.map( (item, index) => (<Product key={item} numero={index} nome={item} />))}
         </>
     )
 }
