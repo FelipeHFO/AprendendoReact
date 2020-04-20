8a4917289d79f25ddd441f644447503bd20f7e36
Felipe Henrique <felipe.henrique.f.o@gmail.com>
2020-04-20 11:06:34 -0300

    Utilizando useEffect para executar um select

--- src/components/productList/productList.js
+++ src/components/productList/productList.js
@@ -1,14 +1,28 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Product from "../product/product";
+import './productList.css'
 
 const ProductList = () => {
 
-    const [itens] = useState(['Pasta de dente', 'Shampoo', 'Sabonete', 'Detergente'])
-    const [categoria] = useState(['Produtos higiênicos', 'Produtos de Limpeza'])
+    const [itens] = useState([['Pasta de dente', 'Shampoo', 'Sabonete'], ['Detergente']])
+    const [itensCategoriaSelecionada, setItensCategoriaSelecionada] = useState([])
+    const [categorias] = useState(['Produtos higiênicos', 'Produtos de Limpeza'])
+    const [categoriasSelecionadas, setCategoriasSelecionadas] = useState('')
+
+    useEffect(()=>{
+        if(categoriasSelecionadas === categorias[0])
+            setItensCategoriaSelecionada(itens[0])
+        else
+            setItensCategoriaSelecionada(itens[1])
+    }, [categoriasSelecionadas])
 
     return(
         <>
-            {itens.map( (item, index) => (<Product numero={index} nome={item} />))}
+            <select onChange={(event) => setCategoriasSelecionadas(event.target.value)}> {/* O evento 'onChange' é do React, ele recebe um evento como parâmetro */}
+                {categorias.map((categoria) => (<option key={categoria} value={categoria}>{categoria}</option>))}
+            </select>
+            
+            {itensCategoriaSelecionada.map( (item, index) => (<Product key={item} numero={index} nome={item} />))}
         </>
     )
 }
